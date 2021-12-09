0f12e62d49089d7722e413fd217c01004c817b80
acharara04 <acharara04pc@gmail.com>
2021-12-09 13:33:47 -0500

    added mixed and duplicate tests

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -11,7 +11,7 @@ testDuplicates();
 }
     public testSelectionSort() {
     }
-    
+
     public void testPositive(){
         int[] arr = new int[5];
         arr[0] = 8;
@@ -19,29 +19,67 @@ testDuplicates();
         arr[2] = 7;
         arr[3] = 10;
         arr[4] = 2;
-        
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = 2;
         Sortedarr[1] = 7;
         Sortedarr[2] = 8;
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
-        
+
         /** add tests to check for this unit test **/
     }
-    
+
     public void testNegative(){
-        
+
         /** Test data contains negative values only **/
     }
-    
+
     public void testMixed(){
-        
-        /** Test data contains with both positive, negative and zeros **/
+
+        int[] arr = new int[5];
+        arr[0] = 8;
+        arr[1] = -9;
+        arr[2] = 0;
+        arr[3] = -10;
+        arr[4] = 2;
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = 0;
+        Sortedarr[3] = 2;
+        Sortedarr[4] = 8;
+
+        int[] newarr = new int[5];
+        newarr = SelectionSort.basicSelectionSort(arr);
+
+        for(int i=0; i<arr.length; i++) {
+        	assertEquals("MIX ERROR AT INDEX " + i, Sortedarr[i], newarr[i]);
+        }
     }
-    
+
     public void testDuplicates(){
-        
-        /** Test data contains duplicates **/
+
+        int[] arr = new int[5];
+        arr[0] = -8;
+        arr[1] = 9;
+        arr[2] = 2;
+        arr[3] = -8;
+        arr[4] = 2;
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -8;
+        Sortedarr[1] = -8;
+        Sortedarr[2] = 2;
+        Sortedarr[3] = 2;
+        Sortedarr[4] = 9;
+
+        int[] newarr = new int[5];
+        newarr = SelectionSort.basicSelectionSort(arr);
+
+        for(int i=0; i<arr.length; i++) {
+        	assertEquals("DUP ERROR AT INDEX " + i, Sortedarr[i], newarr[i]);
+        }
     }
 }
